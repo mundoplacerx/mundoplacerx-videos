05bc0e2ab190823163f4b33f1a274482c31efb7c
mundoplacerx <contacto.mundoplacerx@gmail.com>
2025-04-30 01:55:04 -0500

    Update videos-mejores-escenas.js

--- videos-mejores-escenas.js
+++ videos-mejores-escenas.js
@@ -17,6 +17,12 @@ const videos = [
     banner: `<script type="text/javascript">juicy_adzone = '1089716';</script>
              <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
   },
+    {
+    titulo: "Jerking Off Using Stepmoms Shampoo - Rachael Cavalli",
+    iframe: `<iframe src="https://streamtape.com/e/mw3W8bMrGbtb3Px/" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
 ];
 
 // Reversa para mostrar primero los más nuevos
