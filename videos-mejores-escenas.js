dfc50039ec1bb5f34ecb73b5b6208e37ba208110
mundoplacerx <contacto.mundoplacerx@gmail.com>
2025-04-29 23:23:56 -0500

    Update videos-mejores-escenas.js

--- videos-mejores-escenas.js
+++ videos-mejores-escenas.js
@@ -23,6 +23,13 @@ const videos = [
     banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
              <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
   }
+  {
+    title: "My dad’s hot girlfriend - Eva Angelina",
+    banner: `<script type="text/javascript">juicy_adzone = '1089716';</script>
+  <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`,
+    iframe: `<iframe src="https://streamtape.com/e/dZoXMemOQysGR1/" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"
+      style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1;"></iframe>`
+  },
 ];
 
 // Reversa para mostrar primero los más nuevos
