0fe6af6ac83b01af01abfa91766e90255fccde9b
mundoplacerx <contacto.mundoplacerx@gmail.com>
2025-04-30 23:58:06 -0500

    Update videos-subtitulados.js

--- videos-subtitulados.js
+++ videos-subtitulados.js
@@ -75,7 +75,7 @@ const videosSubtitulados = [
       "https://i.postimg.cc/DWhKW9J3/adira-2.png",
       "https://i.postimg.cc/TKg63dPc/adira-3.png",
     ],
-    iframe: `<iframe src="https://voe.sx/e/iyoz4emoqshq" width="560" height="315" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
+    iframe: `<iframe src="https://voe.sx/e/iyoz4emoqshq" width="800" height="600" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
     banner: `<script type="text/javascript">juicy_adzone = '1089716';</script>
              <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
   },
