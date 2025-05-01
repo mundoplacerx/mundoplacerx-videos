5472dde6ceb3135a7f4d3fbf30f1515baed69554
mundoplacerx <contacto.mundoplacerx@gmail.com>
2025-04-30 22:51:18 -0500

    Create buscador.js

---
+++ buscador.js
@@ -0,0 +1,153 @@
+// videos-mejores-escenas.js
+const videos = [
+  {
+    titulo: "My dad’s hot girlfriend - Eva Angelina",
+    iframe: `<iframe src="https://streamtape.com/e/dZoXMemOQysGR1/" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089716';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "Pullout Training With Girlfriends Stepmom - Charlie Forde",
+    iframe: `<iframe src="https://streamtape.com/e/AAKBMp9Y1QIXA8b/" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "Step Mom Wants To Get Horny - Cherie Deville",
+    iframe: `<iframe src="https://streamtape.com/e/px096q9jvpcWkk/" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089716';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "Jerking Off Using Stepmoms Shampoo - Rachael Cavalli",
+    iframe: `<iframe src="https://streamtape.com/e/mw3W8bMrGbtb3Px/" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "A goal into stepmom's pussy - Richelle Ryan",
+    iframe: `<iframe src="https://streamtape.com/e/bZWr9WOkKBUPPAz/" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+];
+
+// videos-subtitulados.js
+const videosSubtitulados = [
+  {
+    titulo: "Entrenamiento con la madrastra de mi novia - Subtitulado al español",
+    actriz: "Charlie Forde",
+    sinopsis: `Charlie es la madrastra de mi novia... ¡y lo mejor, con subtítulos en español!`,
+    imagenes: [
+      "https://i.postimg.cc/HjWWLs1Y/charlie-1.jpg",
+      "https://i.postimg.cc/BthSP9Jx/charlie-2.jpg",
+      "https://i.postimg.cc/tYw9D3ZJ/charlie-3.jpg",
+      "https://i.postimg.cc/kBBnx15L/charlie-4.jpg"
+    ],
+    iframe: `<iframe src="https://streamtape.com/e/02j0QQGkkRfb986/" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"
+    style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1;"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "La novia caliente de mi papá - Subtitulado al español",
+    actriz: "Eva Angelina y Xander Corvus",
+    sinopsis: `Eva Angelina lleva meses intentando conseguirle al hijo de su esposo... romance prohibido.`,
+    imagenes: [
+      "https://i.postimg.cc/zH5vNFDk/eva-angelina-1.jpg",
+      "https://i.postimg.cc/tn5T4nYG/eva-angelina-2.jpg",
+      "https://i.postimg.cc/hhbbdCmc/eva-angelina-3.jpg",
+      "https://i.postimg.cc/t7HN70YB/eva-angelina-4.jpg",
+      "https://i.postimg.cc/FkzMj68s/eva-angelina-5.jpg",
+      "https://i.postimg.cc/bdFczzSG/eva-angelina-6.jpg"
+    ],
+    iframe: `<iframe src="https://streamtape.com/e/j6RegRLjL4umZj/" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089716';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "La madrastra quiere ponerse Caliente - Cherie Deville",
+    actriz: "Cherie Deville",
+    sinopsis: `Ella estaba lista para devolverme el favor... lo mejor con subtitulos en español.`,
+    iframe: `<iframe src="https://streamtape.com/e/O7LPkzaW1dfZvq1/" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089716';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "Un gol en el coño de la madrastra - Richelle Ryan",
+    actriz: "Richelle Ryan",
+    sinopsis: `A la MILF Richelle Ryan le encanta relajarse... con subtitulos en español.`,
+    imagenes: [
+      "https://i.postimg.cc/LYgZ0gDp/Richelle-1.jpg",
+      "https://i.postimg.cc/NyJ1ncw1/richelle-2.png",
+      "https://i.postimg.cc/NyJ1ncw1/richelle-2.png"
+    ],
+    iframe: `<iframe src="https://streamtape.com/e/O170b662lrHG6j/" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089716';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "Masturbarse con el champú de madrastra - Rachael Cavalli",
+    actriz: "Rachael Cavalli",
+    sinopsis: `Rachel Cavalli se ha quedado sin champú otra vez... lo mejor con subtítulos en español.`,
+    imagenes: [
+      "https://i.postimg.cc/dhGBr00W/Rachaell-1.jpg",
+      "https://i.postimg.cc/nsZ1ktjn/rachaell-2.jpg",
+      "https://i.postimg.cc/kVcvYhKK/rachaell-3.jpg"
+    ],
+    iframe: `<iframe src="https://voe.sx/e/csnj4sumknr7" width="800" height="600" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089716';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+];
+
+// Unir todo en un solo array para el buscador
+const todosLosVideos = [...videos, ...videosSubtitulados];
+
+// Función para renderizar los resultados
+function renderizarResultados(resultados) {
+  const contenedor = document.getElementById('resultados');
+  contenedor.innerHTML = ''; // Limpiar resultados anteriores
+
+  if (resultados.length === 0) {
+    contenedor.innerHTML = '<p>No se encontraron resultados.</p>';
+    return;
+  }
+
+  resultados.forEach(video => {
+    const tarjeta = document.createElement('div');
+    tarjeta.classList.add('tarjeta-video');
+
+    const titulo = document.createElement('h3');
+    titulo.textContent = video.titulo;
+
+    const iframe = document.createElement('div');
+    iframe.innerHTML = video.iframe;
+
+    const banner = document.createElement('div');
+    banner.innerHTML = video.banner;
+
+    tarjeta.appendChild(titulo);
+    tarjeta.appendChild(iframe);
+    tarjeta.appendChild(banner);
+
+    contenedor.appendChild(tarjeta);
+  });
+}
+
+// Función de búsqueda
+function buscarVideos(termino) {
+  const terminoMinuscula = termino.toLowerCase();
+  const resultados = todosLosVideos.filter(video =>
+    video.titulo.toLowerCase().includes(terminoMinuscula)
+  );
+  renderizarResultados(resultados);
+}
+
+// Evento para el input de búsqueda
+document.addEventListener('DOMContentLoaded', () => {
+  const inputBusqueda = document.getElementById('input-busqueda');
+  inputBusqueda.addEventListener('input', (e) => {
+    buscarVideos(e.target.value);
+  });
+});
