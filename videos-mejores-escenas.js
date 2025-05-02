ed403cbfa9176b47cb6f4589d45f163ebb281a4d
mundoplacerx <contacto.mundoplacerx@gmail.com>
2025-05-01 23:33:31 -0500

    Update videos-mejores-escenas.js

--- videos-mejores-escenas.js
+++ videos-mejores-escenas.js
@@ -53,6 +53,12 @@ const videos = [
     banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
              <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
   },
+   {
+    titulo: "Payton Preslee - Use Me",
+    iframe: `<iframe src="https://voe.sx/e/hxtpw6t9dvcr" width="800" height="600" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
 ];
 
 // Reversa para mostrar primero los más nuevos
