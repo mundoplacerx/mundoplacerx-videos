74a96c18b7a8716eca95d238ec0770262f63934d
mundoplacerx <contacto.mundoplacerx@gmail.com>
2025-05-04 10:24:22 -0500

    Update videos-mejores-escenas.js

--- videos-mejores-escenas.js
+++ videos-mejores-escenas.js
@@ -95,6 +95,24 @@ const videos = [
     banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
              <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
   },
+  {
+    titulo: "Jane Cane - Free Use Mom",
+    iframe: `<iframe src="https://voe.sx/e/tqea63byhw0s" width="800" height="600" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "Eva Long - Its A Mommy Thing Vol 8",
+    iframe: `<iframe src="https://voe.sx/e/dvx8mkaji8ck" width="800" height="600" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "Kendra Lust - is My Friends Hot Mom Alan is her sons friend.",
+    iframe: `<iframe src="https://voe.sx/e/p5bfdp76fevf" width="800" height="600" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
 ];
 
 // Reversa para mostrar primero los más nuevos
