19fed847944cd85be6eb915fa0affe606c7c41f5
mundoplacerx <contacto.mundoplacerx@gmail.com>
2025-05-05 12:46:46 -0500

    Update videos-mejores-escenas.js

--- videos-mejores-escenas.js
+++ videos-mejores-escenas.js
@@ -113,6 +113,18 @@ const videos = [
     banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
              <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
   },
+  {
+    titulo: "Sarah Vandella - Sons Selfie Dick.",
+    iframe: `<iframe src="https://voe.sx/e/dj7zgrlfy2dp" width="800" height="600" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
+  {
+    titulo: "Payton Preslee - What We Can Do Inside? 3 - One Last Special Day.",
+    iframe: `<iframe src="https://voe.sx/e/ag1ahzaekldv" width="800" height="600" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
+    banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+             <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+  },
 ];
 
 // Reversa para mostrar primero los más nuevos
