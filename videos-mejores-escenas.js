0dafd3475614fec2859b2e0760c08c14b90b07f2
mundoplacerx <contacto.mundoplacerx@gmail.com>
2025-05-09 13:51:35 -0500

    Update videos-mejores-escenas.js

--- videos-mejores-escenas.js
+++ videos-mejores-escenas.js
@@ -161,6 +161,18 @@ const videos = [
      banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
               <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
    },
+   {
+     titulo: "Eva Long - Moms Rude Awakening.",
+     iframe: `<iframe src="https://voe.sx/e/gh5dcxekcz2h" width="800" height="600" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
+     banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+              <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+   },
+   {
+     titulo: "Helena Price - Say Yes To Undress",
+     iframe: `<iframe src="https://voe.sx/e/k3oq4nfwruny" width="800" height="600" scrolling="no" frameborder="0" allowfullscreen></iframe>`,
+     banner: `<script type="text/javascript">juicy_adzone = '1089718';</script>
+              <script type="text/javascript" src="https://poweredby.jads.co/js/jfc.js" charset="utf-8"></script>`
+   },
 ];
 
 // Reversa para mostrar primero los más nuevos
